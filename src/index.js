700ccdeefaa45ccdea7d9eabdba02959f0974834
Syncxv <aruldevarackal1@gmail.com>
2021-10-08 23:56:20 +0530

    FUCK BRO WORK

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
-console.log("%cSTARTED", "font-size: 24px; color: red")
-const injectCSS = css => {
-    let el = document.createElement('style');
-    el.type = 'text/css';
-    el.innerText = css;
-    document.head.appendChild(el);
-    return el;
-  };
-  
-  injectCSS(`[aria-label="Play on TV"] {
-    display: none;
-  }`)
-  injectCSS(`.ytp-remote-button.ytp-button {
-    display: none;
-  }`)
+
+
+  var checkReady = setInterval(() => {
+    if (document.readyState === "complete") {
+        clearInterval(checkReady) 
+        console.log("%cSTARTED", "font-size: 24px; color: red")
+    const injectCSS = css => {
+        let el = document.createElement('style');
+        el.type = 'text/css';
+        el.innerText = css;
+        document.head.appendChild(el);
+        return el;
+      };
+      
+      injectCSS(`[aria-label="Play on TV"] {
+        display: none;
+      }`)
+      injectCSS(`.ytp-remote-button.ytp-button {
+        display: none;
+      }`)
+
+    }
+  })
