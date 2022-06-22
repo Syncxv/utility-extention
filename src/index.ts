b77b9511a92808869b64e8ff99f829214218402d
Syncxv <aruldevarackal1@gmail.com>
2022-06-22 18:38:28 +0530

    you know how it is

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
-import osumain from "./osuCollector/index";
-
+import { nsfwBlockerMain } from './nsfwBlocker';
+import osumain from './osuCollector/index';
+console.clear = () => {};
 var checkReady = setInterval(() => {
-    if (document.readyState === "complete") {
+    if (document.readyState === 'complete') {
         clearInterval(checkReady);
-        console.log("%cSTARTED", "font-size: 24px; color: red");
-
+        console.log('%cSTARTED', 'font-size: 24px; color: red');
+        nsfwBlockerMain();
         // for osucollector
-        if (location.href.includes("https://osucollector.com/collections")) {
+        if (location.href.includes('https://osucollector.com/collections')) {
             osumain();
         }
     }
---
+++ src/nsfwBlocker/index.ts
@@ -0,0 +1,59 @@
+// import sleep from '../util/sleep';
+
+// const badWebsites = [
+//     'pornhub.com',
+//     'pornhub.org',
+//     'pixiv.net',
+//     'redgifs.com',
+//     'hanime.tv',
+//     'nhentai.com',
+//     'gelbooru.com',
+//     'fakku.net',
+//     'rule34.xxx',
+//     'hitomi.la',
+//     'e-hentai.org',
+//     'nhentai.com/home',
+// ];
+const fasterBadWebsites: { [key: string]: boolean } = {
+    'pornhub.com': true,
+    'pornhub.org': true,
+    'pixiv.net': true,
+    'redgifs.com': true,
+    'hanime.tv': true,
+    'nhentai.com': true,
+    'gelbooru.com': true,
+    'fakku.net': true,
+    'rule34.xxx': true,
+    'hitomi.la': true,
+    'e-hentai.org': true,
+    'nhentai.com/home': true,
+};
+export const nsfwBlockerMain = async () => {
+    console.log('Hi');
+    console.clear = () => {};
+    const real = location.hostname.replace('www.', '');
+
+    if (fasterBadWebsites[real]) {
+        console.log("you're being very bad");
+        document.body.innerHTML = '';
+        document.body.style.display = 'flex';
+        document.body.style.alignItems = 'center';
+        document.body.style.justifyContent = 'center';
+        document.body.style.height = '100vh';
+        document.body.append('GET BACK ON THE GRIND KIND SIR');
+    }
+
+    // badWebsites.forEach((s) => {
+    //     const real = location.hostname.replace('www.', '');
+    //     console.log(s, real);
+    //     if (real === s) {
+    //         console.log("you're being very bad");
+    //         document.body.innerHTML = '';
+    //         document.body.style.display = 'flex';
+    //         document.body.style.alignItems = 'center';
+    //         document.body.style.justifyContent = 'center';
+    //         document.body.style.height = '100vh';
+    //         document.body.append('GET BACK ON THE GRIND KIND SIR');
+    //     }
+    // });
+};
