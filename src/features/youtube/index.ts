a771fceaaca35743f66c64caf81f9847365ea3f6
Syncxv <aruldevarackal1@gmail.com>
2022-06-22 19:45:42 +0530

    whoops

--- src/features/youtube/index.ts
+++ src/features/youtube/index.ts
@@ -1,12 +1,12 @@
-import injectCSS from "../util/injectCss";
+import injectCSS from '../../util/injectCss'
 
 const youtubeMain = () => {
     injectCSS(`[aria-label="Play on TV"] {
         display: none;
-      }`);
+      }`)
     injectCSS(`.ytp-remote-button.ytp-button {
         display: none;
-      }`);
-};
+      }`)
+}
 
-export default youtubeMain;
+export default youtubeMain
